adfb83e082dea935884cf55b257b0d19d594f2ac
Ahmet Tumis <ahmettumis@gmail.com>
2024-12-24 15:56:53 +0300

    client and server initial implementation added

--- client/client.cpp
+++ client/client.cpp
@@ -2,22 +2,98 @@
 #include <fstream>
 #include <string>
 #include <vector>
-#include <netinet/in.h>
+#include <filesystem>
+#include <sys/socket.h>
+#include <arpa/inet.h>
 #include <unistd.h>
 #include "checksum.cpp"
 
-// std::string CalculateChecksum(const std::vector<char>& data, size_t size);
-
 //STOR
-void UploadFile(int clientFd, const std::string& filePath){};
-//RETR
-void DownloadFile(int clientFd, const std::string& fileName){};
+void UploadFile(int clientFd, const std::string& filePath){
 
-int main(){
-    std::cout << "Connected to FTP server\n";
-    std::string testStr = "TEST";
-    std::vector<char> data(testStr.begin(), testStr.end());
-    auto result = CalculateChecksum(data, data.size());
-    std::cout << "Hash result: " << result << "\n";
-    return 0;
+    std::ifstream file(filePath, std::ios::binary);
+    if (!file) {
+        std::cerr << "File not found: " << filePath << "\n";
+        return;
+    }
+
+    std::vector<char> fileData((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
+    std::string filename = filePath.substr(filePath.find_last_of("/\\") + 1);
+    std::string checksum = CalculateChecksum(fileData, fileData.size());
+
+    std::string command = "STOR " + filename;
+    send(clientFd, command.c_str(), command.size(), 0);
+
+    send(clientFd, checksum.c_str(), checksum.size(), 0);
+    send(clientFd, "\n", 1, 0); // what about \0
+
+    send(clientFd, fileData.data(), fileData.size(), 0);
+    std::cout << "File Uploaded\n";
 };
+//RETR
+void DownloadFile(int clientFd, const std::string& fileName){
+    std::string command = "RETR " + fileName;
+    send(clientFd, command.c_str(), command.size(), 0);
+
+    char checksumBuffer[65] = {0};
+    read(clientFd, checksumBuffer, 64); // Receive checksum
+    std::cout << "Received checksum: " << checksumBuffer << "\n";
+    std::filesystem::create_directory("./received/");
+    std::ofstream file("./received/" + fileName, std::ios::binary);
+
+    std::vector<char> fileData(4096);
+    ssize_t bytesReceived;
+
+    while ((bytesReceived = read(clientFd, fileData.data(), fileData.size())) > 0) {
+        file.write(fileData.data(), bytesReceived);
+    }
+
+    std::string calculatedChecksum = CalculateChecksum(fileData, fileData.size());
+    if (calculatedChecksum == checksumBuffer) {
+        std::cout << "File downloaded successfully with matching checksum.\n";
+    } else {
+        std::cerr << "Checksum mismatch for file: " << fileName << "\n";
+    }
+};
+
+int main() {
+    const size_t chunkSize = 4096;
+    int clientFd = socket(AF_INET, SOCK_STREAM, 0);
+    sockaddr_in serverAddr = {AF_INET, htons(2102)};
+    inet_pton(AF_INET, "127.0.0.1", &serverAddr.sin_addr);
+
+    connect(clientFd, (sockaddr*)&serverAddr, sizeof(serverAddr));
+    std::cout << "Connected to FTP server\n";
+
+    while (true) {
+        std::string command, filepath;
+        std::cout << "ftp> ";
+        std::getline(std::cin, command);
+
+        if (command.substr(0, 4) == "LIST") {
+            std::vector<char> buffer(chunkSize);
+            ssize_t bytesReceived;
+            send(clientFd, command.c_str(), command.size(), 0);
+            std::cout << "Directory Listing:\n";
+            while ((bytesReceived = read(clientFd, buffer.data(), buffer.size())) > 0) {
+                std::cout.write(buffer.data(), bytesReceived);
+                if (bytesReceived < static_cast<ssize_t>(chunkSize)) break;
+            }
+            std::cout << "\n";
+
+        } else if (command.substr(0, 4) == "STOR") {
+            std::cout << "Enter file path: ";
+            std::getline(std::cin, filepath);
+            UploadFile(clientFd, filepath);
+        } else if (command.substr(0, 4) == "RETR") {
+            std::cout << "Enter filename: ";
+            std::getline(std::cin, filepath);
+            DownloadFile(clientFd, filepath);
+        } else {
+            send(clientFd, command.c_str(), command.size(), 0);
+        }
+    }
+
+    close(clientFd);
+    return 0;
+}
--- server/server.cpp
+++ server/server.cpp
@@ -4,16 +4,105 @@
 #include <vector>
 #include <netinet/in.h>
 #include <unistd.h>
+#include <filesystem>
+#include <dirent.h>
 #include "checksum.cpp"
 
+void HandleClient(int clientSocket){
 
-void HandleClient(int clientSocket){};
+    const std::string baseDir = "./hosted_files/";
+    const size_t chunkSize = 4096; //make this changable
+    
+    while(true){
+
+        std::vector<char> buffer(chunkSize);
+        ssize_t bytesReceived = read(clientSocket, buffer.data(), buffer.size());
+        std::cout << "Message received\n";
+        if(bytesReceived <= 0)
+            break;
+        
+        std::string command(buffer.data(), bytesReceived);
+
+        if(command.substr(0,4) == "LIST"){
+            // List files in directory
+            DIR* dir = opendir(baseDir.c_str());
+            if (!dir) {
+                std::string err = "550 Failed to open directory\n";
+                send(clientSocket, err.c_str(), err.size(), 0);
+            } else {
+                std::string file_list;
+                struct dirent* entry;
+                while ((entry = readdir(dir)) != nullptr) {
+                    if (entry->d_name[0] != '.') { // Skip hidden files
+                        file_list += entry->d_name;
+                        file_list += "\n";
+                    }
+                }
+                closedir(dir);
+                std::cout << "File list is: " << file_list << "\n";
+                send(clientSocket, file_list.c_str(), file_list.size(), 0);
+            }
+
+        } else if(command.substr(0,4) == "RETR"){
+            std::string fileName = baseDir + command.substr(5);
+            std::ifstream file(fileName, std::ios::binary);
+
+            if(!file){
+                std::string err = "550 File Not Found\n";
+                send(clientSocket, err.c_str(), err.size(), 0);
+            } else {
+                std::vector<char> fileData((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
+                std::string checksum = CalculateChecksum(fileData, fileData.size());
+                std::cout << "Calculated checksum for: " << command.substr(5) << "::" << checksum << "\n";
+                send(clientSocket, checksum.c_str(), checksum.size(), 0);
+                send(clientSocket, "\n", 1, 0);
+                send(clientSocket, fileData.data(), fileData.size(), 0);
+            }
+
+        } else if (command.substr(0,4) == "STOR"){
+        std::string filename = baseDir + command.substr(5);
+            std::ofstream file(filename, std::ios::binary);
+
+            char checksumBuffer[65] = {0};
+            read(clientSocket, checksumBuffer, 64); // Receive checksum
+
+            std::vector<char> fileData;
+            while ((bytesReceived = read(clientSocket, buffer.data(), buffer.size())) > 0) {
+                fileData.insert(fileData.end(), buffer.begin(), buffer.begin() + bytesReceived);
+                if (bytesReceived < static_cast<ssize_t>(chunkSize))
+                    break;
+            }
+
+            std::string calculatedChecksum = CalculateChecksum(fileData, fileData.size());
+            if (calculatedChecksum == checksumBuffer) {
+                file.write(fileData.data(), fileData.size());
+                std::string msg = "200 OK\n";
+                send(clientSocket, msg.c_str(), msg.size(), 0);
+            } else {
+                std::string msg = "550 Checksum Mismatch\n";
+                send(clientSocket, msg.c_str(), msg.size(), 0);
+            }
+        } else {
+            std::string msg = "500 Unknown Command\n";
+            send(clientSocket, msg.c_str(), msg.size(), 0);
+        }
+
+    }
+};
 
 int main(){
-    std::cout << "FTP server listening\n";
-    std::string testStr = "TEST";
-    std::vector<char> data(testStr.begin(), testStr.end());
-    auto result = CalculateChecksum(data, data.size());
-    std::cout << "Hash result: " << result << "\n";
+
+    int serverFd = socket(AF_INET, SOCK_STREAM, 0);
+    sockaddr_in serverAddr = {AF_INET, htons(2102), INADDR_ANY};
+
+    bind(serverFd, (sockaddr*)&serverAddr, sizeof(serverAddr));
+    listen(serverFd, 1);
+    std::cout << "FTP server listening on port 2102...\n";
+
+    int client_socket = accept(serverFd, nullptr, nullptr);
+    HandleClient(client_socket);
+
+    close(client_socket);
+    close(serverFd);
     return 0;
 }
